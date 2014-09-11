90c968f81aec066c567c769135e827c15b602b6c
Wilbert van Ham <Wilbert.vanHam@gmail.com>
2014-09-11 09:37:44 +0200

    zNear, zFar, dEyes => exp. variables

--- field.py
+++ field.py
@@ -327,10 +327,21 @@ class Field(QGLWidget):
 		# set uniform variables and set up VBO's for the attribute values
 		# reference triangles, do not move in model coordinates
 		# position of the center
-		self.dEyes = self.conditions.getNumber('dEyes')
-		self.zNear = self.conditions.getNumber('zNear')
-		self.zFar = self.conditions.getNumber('zFar')
+		try:
+			self.dEyes = self.conditions.trial['dEyes']
+		except:
+			logging.info('could not get dEyes from experiment file')
+			
+		try:
+			self.zNear = self.conditions.trial['zNear']
+		except:
+			logging.info('could not get zNear from experiment file')
 
+		try:
+			self.zfar = self.conditions.trial['zFar']
+		except:
+			logging.info('could not get zFar from experiment file')
+			
 		
 		self.nMD  = self.conditions.getNumber('nMD'+moveString)
 		self.nMND = self.conditions.getNumber('nMND'+moveString)
